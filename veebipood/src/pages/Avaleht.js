ae17340d7006392510ea6e69142f9cf923cc8e65
KristoVunukainen <81011724+KristoVunukainen@users.noreply.github.com>
2023-06-29 20:36:07 +0300

    29.06.2023

--- veebipood/src/pages/Avaleht.js
+++ veebipood/src/pages/Avaleht.js
@@ -1,13 +1,43 @@
 // Simple React Snippets
 //rfce
 
-import React from 'react'
+import React, { useState } from "react";
 
 function Avaleht() {
+  const [kogus, uuendaKogus] = useState(0);
+  const [laigitud, uuendaLaigitud] = useState(false);
+  const [sonum, uuendaSonum] = useState ("Uuenda kogust!");
+
+  function nulli (){
+    uuendaKogus(0);
+    uuendaSonum ("Panid koguse nulli!");
+  }
+  function vahenda (){
+    uuendaKogus(kogus -1);
+    uuendaSonum ("Vähendasid kogust!");
+  }
+  function suurenda (){
+    uuendaKogus(kogus +1);
+    uuendaSonum ("Suurendasid kogust!");
+  }
+
   return (
-    <div>Avaleht</div>
-  )
-  
+    <div>
+      {laigitud === true && <img src="/laigitud.svg" alt="" />}
+      {laigitud === false && <img src="/mitte laigitud.svg" alt="" />}
+      {laigitud}
+      <button onClick={() => uuendaLaigitud (!laigitud)}>muuda like-i</button>
+      {/* <button onClick={() => uuendaLaigitud(true)}>Muuda laigituks</button> */}
+      {/* <button onClick={() => uuendaLaigitud(false)}>Muuda mitte laigituks</button> */}
+      <br />
+      <br />
+       <div>{sonum}</div>
+      {kogus > 0 && (<button onClick={nulli} >Tagasi nulli</button>)}
+      <button disabled ={kogus ===0} onClick={() => vahenda(kogus - 1)}>-</button>
+      {kogus}
+      <button onClick={() => suurenda(kogus + 1)}>+</button>
+    </div>
+  );
 }
 
-export default Avaleht
+export default Avaleht;
--- veebipood/src/pages/LisaToode.js
+++ veebipood/src/pages/LisaToode.js
@@ -1,9 +1,46 @@
-import React from 'react'
+
+import React, { useRef, useState } from 'react'
 
 function LisaToode() {
+const [sonum, uuendaSonum] = useState();
+const inputiLuger = useRef();
+const lisa = ()=> {
+  if (inputiLuger.current.value ==="") {
+    uuendaSonum ("Tühja nimetusega toodet ei saa lisada!")
+
+  } else {
+    uuendaSonum("Toode lisatud: " + inputiLuger.current.value )
+  }
+
+  
+}
+
   return (
-    <div>LisaToode</div>
+    <div>
+      <div>{sonum}</div>
+      <label>Toote nimi</label> <br />
+      <input ref={inputiLuger} type="text" /> <br />
+      <button onClick={lisa}>Lisa</button><br />
+    </div>
   )
 }
 
 export default LisaToode
+
+
+  // [] - useState võrdusmärgist vasakul pool, esimene on muutuja, teine on millega teda muuta
+  // {} - JS-s koodiblokk, HTML-s dünaamika tekitaja
+  // () - funktsioonide väljakutsumiseks   if () <--- if'i tingimuse lugemiseks
+  // =  - väärtuse andmiseks
+  // === - võrdluseks kas vasak ja parem pool on identsed
+  // >  <   >=   - numbrite võrdlemiseks suurem / väiksem / suurem-võrdne
+  // &&  -   kui vasakul pool on tõde, siis näita
+  // ;  - rea lõpetamiseks
+  // ! - keera vastupidiseks     !true ---> false
+  // tumesinine - sissekirjutatud tüüp
+  // roheline - võõras klass
+  // helesinine - muutuja omadused
+  // oranž - jutumärkides väärtus
+  // kollane - funktsioon, taaskasutatav koodiblokk, lõppeb enamustel juhtudel sulgudega
+  // tavaline sinine - muutuja, millel on muutuv väärtus
+  // lilla - import, from, export, if, else, return - sissekirjutatud funktsionaalsus
